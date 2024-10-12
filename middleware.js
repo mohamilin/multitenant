82c7927fa87f806c7b671aacddbae02a9bbfbf49
Moh Amilin <amilinmoh99@gmail.com>
2024-10-12 21:20:41 +0700

    add multitenant-theta.vercel.app

--- middleware.js
+++ middleware.js
@@ -13,7 +13,7 @@ export default async function middleware(req) {
   const hostname = req.headers.get("host");
 
   // Se define una lista de dominios permitidos (incluyendo localhost y el dominio real)
-  const allowedDomains = ["localhost:3000", "tudominio.com"];
+  const allowedDomains = ["localhost:3000", "tudominio.com", "multitenant-theta.vercel.app"];
 
   // Verificamos si el hostname actual está en la lista de dominios permitidos
   const isAllowedDomain = allowedDomains.some(domain => hostname.includes(domain));
